8fe8c38eb4deb203d98723eebacd42443971c184
heineiuo <heineiuo@gmail.com>
2022-05-13 19:51:30 +0800

    Add kinetic scrolling for touch screen device

--- src/VirtualizedGrid.tsx
+++ src/VirtualizedGrid.tsx
@@ -406,23 +406,23 @@ export function VirtualizedGrid({
 
   const onWheel = useMemo(() => {
     let timer = null;
-    let overlayX = 0;
-    let overlayY = 0;
+    let totalX = 0;
+    let totalY = 0;
     return (event) => {
       if (!onWheelThrottled) {
         return updateCoordinate(event);
       }
       clearTimeout(timer);
       const { deltaX, deltaY } = event;
-      overlayX += deltaX;
-      overlayY += deltaY;
+      totalX += deltaX;
+      totalY += deltaY;
       timer = setTimeout(() => {
         updateCoordinate({
-          deltaX: overlayX,
-          deltaY: overlayY,
+          deltaX: totalX,
+          deltaY: totalY,
         });
-        overlayX = 0;
-        overlayY = 0;
+        totalX = 0;
+        totalY = 0;
       }, 0);
     };
   }, [updateCoordinate]);
@@ -456,22 +456,109 @@ export function VirtualizedGrid({
     }
   }, [onWheel]);
 
+  /**
+   * pan responder for touch screen device
+   *
+   * The kinetic scrolling algorithm inspired by Ariya
+   * https://ariya.io/2013/11/javascript-kinetic-scrolling-part-2
+   */
   const panResponder = useMemo(() => {
     let prevGestureState = null;
+    let ticker = null;
+    let deltaX = 0;
+    let deltaY = 0;
+
+    /**
+     * for track()
+     */
+    let totalX = 0;
+    let totalY = 0;
+    let timestamp = Date.now();
+    /**
+     * 水平速度
+     */
+    let vx = 0;
+    /**
+     * 垂直速度
+     */
+    let vy = 0;
+
+    /**
+     * for autoScroll
+     */
+    let finalX = 0;
+    let finalY = 0;
+
+    const timeConstant = 325; // ms
+
+    function init() {
+      finalX = 0;
+      finalY = 0;
+      totalX = 0;
+      totalY = 0;
+      vx = 0;
+      vy = 0;
+      timestamp = Date.now();
+      clearInterval(ticker);
+      ticker = setInterval(track, 100);
+    }
+
+    /**
+     * 计算速度
+     *
+     * 实时速度 = 1000 * 位移 / 时间间隔 + 1
+     * 速度(移动平均值) = 实时速度 * 0.8 + 速度前值 * 0.2
+     */
+    function track() {
+      if (__DEV__) {
+        console.time("track");
+      }
+      const now = Date.now();
+      const elapsed = now - timestamp + 1;
+      timestamp = now;
+      const velocityX = (1000 * totalX) / elapsed;
+      const velocityY = (1000 * totalY) / elapsed;
+      vx = velocityX * 0.8 + vx * 0.2;
+      vy = velocityY * 0.8 + vy * 0.2;
+
+      totalX = 0;
+      totalY = 0;
+      if (__DEV__) {
+        console.timeEnd("track");
+      }
+    }
+
+    function autoScroll() {
+      const elapsed = Date.now() - timestamp;
+      const per = Math.exp(-elapsed / timeConstant);
+      const deltaX = finalX * per;
+      const deltaY = finalY * per;
+      if (deltaX > 0.5 || deltaX < -0.5 || deltaY > 0.5 || deltaY < -0.5) {
+        updateCoordinate({ deltaX, deltaY });
+        requestAnimationFrame(autoScroll);
+      } else {
+        updateCoordinate({ deltaX, deltaY });
+      }
+    }
+
     return PanResponder.create({
-      onPanResponderGrant: (event, gestureState) => {
-        prevGestureState = { ...gestureState };
-      },
       onMoveShouldSetPanResponder: () => {
         return true;
+      },
+      onPanResponderGrant: (event, gestureState) => {
+        prevGestureState = { ...gestureState };
+        init();
       },
       onPanResponderMove: (event, gestureState) => {
         if (!prevGestureState) {
           prevGestureState = { ...gestureState };
+          init();
           return;
         }
-        const deltaX = -gestureState.dx + prevGestureState.dx;
-        const deltaY = -gestureState.dy + prevGestureState.dy;
+        deltaX = -gestureState.dx + prevGestureState.dx;
+        deltaY = -gestureState.dy + prevGestureState.dy;
+        totalX = deltaX;
+        totalY = deltaY;
         prevGestureState = { ...gestureState };
         updateCoordinate({
           deltaX,
@@ -480,6 +567,15 @@ export function VirtualizedGrid({
       },
       onPanResponderRelease: () => {
         prevGestureState = null;
+        clearInterval(ticker);
+        if (vx > 10 || vx < -10) {
+          finalX = 0.8 * vx;
+        }
+        if (vy > 10 || vy < -10) {
+          finalY = 0.8 * vy;
+        }
+        timestamp = Date.now();
+        requestAnimationFrame(autoScroll);
       },
     });
   }, [updateCoordinate]);
